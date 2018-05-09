7574ae8cd3ffb2cd2b869027a108385943673550
Gabriel Campos Albino <gagocampos@localhost.localdomain>
2018-05-09 10:29:27 -0300

    Validação de dados de registro do usuário

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,12 +7,24 @@ class HomeController < ApplicationController
     self.params = params.permit!
     @senha = params[:aluno][:senha]
     @conSenha = params[:aluno][:conSenha]
+    @nome = params[:aluno][:nome]
+    @matricula = params[:aluno][:matricula]
+    @email = params[:aluno][:email]
+    @cpf = params[:aluno][:cpf]
+    @rg = params[:aluno][:rg]
+    result3 = Integer(@matricula) rescue false
+    result1 = Integer(@cpf) rescue false
+    result2 = Integer(@rg) rescue false
 
-    if @senha == @conSenha
-      @aluno = Aluno.create!(permitted)
-      flash[:notice] = "Usuário cadastrado com sucesso!"
+    if @nome == "" || @email == "" || @senha == "" || result1 == false || result2 == false || result3 == false
+        flash[:notice] = "Por favor preencha os campos corretamente."
     else
-      flash[:notice] = "Senhas não são iguais. Usuário não cadastrado."
+        if @senha == @conSenha
+          @aluno = Aluno.create!(permitted)
+          flash[:notice] = "Usuário cadastrado com sucesso!"
+        else
+          flash[:notice] = "Senhas não são iguais. Usuário não cadastrado."
+        end
     end
     redirect_to root_path
   end
