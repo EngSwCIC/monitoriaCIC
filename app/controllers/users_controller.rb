f27b810e8f4bd6be4582a4fa3db1f66d55b95bb7
Bernardo Costa Nascimento <bernardoc1104@gmail.com>
2018-05-30 14:40:11 -0300

    Bug fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,26 +11,7 @@ class UsersController < ApplicationController
     @password   = false
 
     @user = User.create!(user_params)
-  rescue ActiveRecord::RecordNotUnique, ActiveRecord::RecordInvalid => err
-    if err.message.include? "users.matricula"
-      @matricula = true
-    elsif err.message.include? "users.email"
-      @email = true
-    elsif err.message.include? "users.cpf"
-      @cpf = true
-    elsif err.message.include? "users.rg"
-      @rg = true
-    elsif err.message.include? "Validation failed"
-      @invalid = true
-    end
-
-    if @matricula || @email || @cpf || @rg || @invalid
-      flash[:notice] = "Registro não pôde ser realizado."
-      redirect_to root_path
-    else
-      flash[:notice] = "Registro realizado com sucesso!"
-      redirect_to root_path
-    end
+    redirect_to root_path
   end
 
   private
