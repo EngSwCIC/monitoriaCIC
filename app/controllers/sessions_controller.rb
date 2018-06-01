7747aacd0878f79bd44b15715004c38e23d1cabf
Bernardo Costa Nascimento <bernardoc1104@gmail.com>
2018-06-01 12:53:55 -0300

    Alterações no login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,22 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user = User.find_by_email(params[:email])
-  	if user && user.authenticate(params[:password])
-  		session[:user_id] = user.email
-  		redirect_to root_url, notice: "Logged In"
+    @data = user_params
+  	@user = User.find_by_email(@data[:email])
+  	if @user && @user.authenticate(@data[:password])
+  		session[:user_id] = @user.email
+  		redirect_to root_url
   	else
-  		redirect_to new_session_path, notice: "Credenciais invalidas"
+  		redirect_to new_session_path, notice: "Email ou senha inválidos"
   	end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, notice: "Logged Out"
+    redirect_to root_url
+  end
+
+  def user_params
+    @data = params.require(:user).permit(:email, :password)
   end
 end
