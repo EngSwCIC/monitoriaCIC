225c4d67316f0be7ff4730d23e757e512d42f44e
Pedro Torres <pedrohagatorres@gmail.com>
2019-06-13 17:16:09 -0300

    Tradução da documentação dos métodos adicionados.

--- app/models/user.rb
+++ app/models/user.rb
@@ -71,14 +71,14 @@ class User < ActiveRecord::Base
     end
   end
 
-  # Sets the password reset attributes.
+  # Define os atributos para resetar a senha
   def create_reset_digest
     self.reset_token = User.new_token
     update_attribute(:reset_digest,  User.digest(reset_token))
     update_attribute(:reset_sent_at, Time.zone.now)
   end
 
-  # Sends password reset email.
+  # Envia o e-mail para resetar a senha
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
   end
