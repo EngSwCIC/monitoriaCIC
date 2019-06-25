819b5cde555ebb2dde6f3e62aa808cdf209cdd8f
Pedro Torres <pedrohagatorres@gmail.com>
2019-06-25 17:12:26 -0300

    Adição do web scraper da lista de professores e refatoramento desse código.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -69,10 +69,77 @@ class DashboardController < ApplicationController
     redirect_to dashboard_apagar_alunos_path
   end
 
+  def importar_professores
+    @teachers = Professor.all
+  end
+
+  def scrape_professores
+    web_scraper
+    redirect_to dashboard_importar_professores_path
+  end
+
   private
   def user_logged
     if !logged_in?
       redirect_to new_session_path, notice: "Você precisa estar logado para acessar essa página"
+    end
+  end
+
+  def web_scraper
+    # Habilitam a abertura de webpages no código
+    require 'open-uri'
+    require 'openssl'
+
+    # Cria um node a partir da página do CIC, filtrando pela 'div' em questão os elementos 'li'. Esse node contém todas
+    # as informações disponíveis de todos os professores
+    teachers_list = Nokogiri::HTML(open('https://cic.unb.br/professores/',
+                                        :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE)).
+        css('div#main-content').
+        css('li')
+
+    teachers_list.each do |t|
+      email = t.css('span.p-email').text.reverse
+
+      if valid_email?(email)
+        unless Professor.find_by_email(email)
+          pwd = generate_password
+          professor = Professor.create(:name => t.css('h4.people-details-h4').text.match(/(\S+\.?\s?)+/),
+                                       :email => email,
+                                       :username => email.match(/[^@]+/),
+                                       :role => match_role(t.css('span.people-details.p-people').text),
+                                       :password => pwd,
+                                       :password_confirmation => pwd)
+          professor.save
+        end
+      end
+    end
+  end
+
+  def valid_email?(email)
+    email =~ /\A[\w+\-.]+@unb\.br\z/i
+  end
+
+  def generate_password
+    require 'securerandom'
+    SecureRandom.base64(9)
+  end
+
+  def match_role(role)
+    case role
+    when /substituto/i
+      1
+    when /colaborador/i
+      3
+    when /adjunto/i
+      4
+    when /associado/i
+      5
+    when /titular/i
+      6
+    when /\A\s/
+      0
+    else
+      2
     end
   end
 end
