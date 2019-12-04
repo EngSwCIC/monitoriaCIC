e62fc21ada46c7f8c8bd9a70e730f9ab5e5937e2
CristianeNaves <cristianenavescardoso09@gmail.com>
2019-12-04 20:02:36 -0300

    colocando () nos factories

--- spec/factories/turma.rb
+++ spec/factories/turma.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :turma do
-    id 1
-    turma 'A'
-    professor 'Genaina Nunes Rodrigues'
-    fk_cod_disciplina 113468
-    fk_status_turma_id 3
-    qnt_bolsas 4
-    fk_vagas_id 1
+    id {1}
+    turma {'A'}
+    professor {'Genaina Nunes Rodrigues'}
+    fk_cod_disciplina {113468}
+    fk_status_turma_id {3}
+    qnt_bolsas {4}
+    fk_vagas_id {1}
   end
 end
