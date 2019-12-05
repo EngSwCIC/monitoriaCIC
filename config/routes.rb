0bcb097010dc01207e9c5668e83ec419160adfe0
CristianeNaves <cristianenavescardoso09@gmail.com>
2019-12-05 11:39:11 -0300

    removendo rota registrar_atividade

--- config/routes.rb
+++ config/routes.rb
@@ -35,7 +35,6 @@ Rails.application.routes.draw do
   match "/dashboard/importar_professores", to: "dashboard#importar_professores", via: 'get'
   match "/dashboard/scrape_professores", to: "dashboard#scrape_professores", via: 'get'
   match "/dashboard/atendimentos", to: "dashboard#atendimentos", via: 'get'
-  match "/dashboard/registrar_atividade", to: "dashboard#registrar_atividade", via: 'get'
   match "/dashboard/atividades", to: "dashboard#atividades", via: 'get'
   match "/faq", to: "faq#index", :via => 'get'
   match "/sobre", to: "sobre#index", :via => 'get'
