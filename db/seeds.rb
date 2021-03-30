7b672f5416e1b98399d6bdaa72b7578282ec2a6a
Icaro Rezende <icaronr@gmail.com>
2021-03-30 10:12:25 -0300

    fix gemfile and migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,26 +17,27 @@ Disciplina.destroy_all
 #    remember_token: nil, created_at: '2018-06-04 18:12:09.050792', updated_at: '2018-06-04 18:12:09.050792')
 
 ######################
-Disciplina.create([{id:1, nome: "APC", 
+Disciplina.create!([{id:1, nome: "APC", 
                     fk_tipo_disciplina_id: 1, c_prat: 2, c_teor: 4, 
                     cod_disciplina: 1}])
 
 
-Disciplina.create([{id:2, nome: "ED", 
+Disciplina.create!([{id:2, nome: "ED", 
                     fk_tipo_disciplina_id: 1, c_prat: 2, c_teor: 2, 
                     cod_disciplina: 2}])
 ######################
 
 
-# User.create(id: 1, name: "Aluno1", email: "aluno1@gmail.com", password: "123456",
-#                 cpf: "03638481182", rg: "2645178", matricula: "140080279")
+User.create!(id: 1, name: "Aluno", email: "aluno1@gmail.com", password: "123456", password_confirmation: '123456',
+                cpf: "03638481182", rg: "2645178", matricula: "140080279")
 
 # User.create(id: 2, name: "Aluno2", email: "aluno2@gmail.com", password: "123456",
 #                 cpf: "03638481111", rg: "2645111", matricula: "140080211",
 #                 fk_banco: nil, remember_token: nil, created_at: '2018-06-04 19:02:24.831795',
 #                 updated_at: '2018-06-04 19:02:24.831795')
 
-#Professor.create(id: 1, name: "Carla", email: "carla@unb.br", username: "carla", password: "123456", role: 1)
+Professor.create!(id: 1, name: "Genaina", email: "genaina@unb.br", username: "genaina", password: "123456", password_confirmation: '123456', role: 1)
+Professor.create!(id: 2, name: "Carla", email: "carla@unb.br", username: "carla", password: "123456", password_confirmation: '123456', role: 1)
 ######################
 
 ######################
