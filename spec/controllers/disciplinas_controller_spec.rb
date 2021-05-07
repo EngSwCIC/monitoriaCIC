21aa6e425bf928bccbd90ee68f3c5962ad8d7f57
titofrota <italoeduardo.df@hotmail.com>
2021-05-07 20:33:49 -0300

    FIX disciplinas_controller_spec

--- spec/controllers/disciplinas_controller_spec.rb
+++ spec/controllers/disciplinas_controller_spec.rb
@@ -204,7 +204,7 @@ describe DisciplinasController do
 
 		describe "DELETE #destroy" do
       before :each do
-        @db_disciplina = FactoryBot.create(:disciplina, id: '1')
+        @db_disciplina = FactoryBot.create(:disciplina, cod_disciplina: '10')
         @params = {}
         @params[:id] = '1'
       end
--- spec/factories/disciplina.rb
+++ spec/factories/disciplina.rb
@@ -1,12 +1,13 @@
 FactoryBot.define do
   factory :disciplina do
-    id {'1'}
+    id {1}
   nome {"Engenharia de Software"}
  	fk_tipo_disciplina_id {1}
  	c_prat {4}
  	c_teor {2}
  	c_est {4}
  	c_ext {0}
+  cod_disciplina {113468}
   monitoria {true}
   end
 end
