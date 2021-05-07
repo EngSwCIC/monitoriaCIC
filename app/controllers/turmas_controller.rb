e283f3afbb3dad64173e5d390c1d66b16758ea49
alexandre-ss <alexandresantana401@gmail.com>
2021-05-07 11:58:57 -0300

    Alterando permissões dashboard admin

--- app/controllers/turmas_controller.rb
+++ app/controllers/turmas_controller.rb
@@ -1,5 +1,5 @@
 class TurmasController < ApplicationController
-  # GET #new
+
   def new
     @user = current_user
   end
@@ -23,9 +23,9 @@ class TurmasController < ApplicationController
   def update
     @turma = Turma.find(params[:id])
 
-    @monitoria = Monitoria.where(fk_turmas_id: params[:id], fk_status_monitoria_id: 3)
-    if @monitoria.length <= turma_params[:qnt_bolsas].to_i || @monitoria.length == 0
-
+    # @monitoria = Monitoria.where(fk_turmas_id: params[:id], fk_status_monitoria_id: 3)
+    # if @monitoria.length <= turma_params[:qnt_bolsas].to_i || @monitoria.length == 0
+    if validate_turma(turma_params[:qnt_bolsas]) == true
       @turma.update_attributes(turma_params)
 
       if !@turma.errors.any?
@@ -78,4 +78,11 @@ class TurmasController < ApplicationController
 				redirect_to dashboard_disciplinas_path
 		end
 	end
+
+  def validate_turma(quantidade)
+    @monitoria = Monitoria.where(fk_turmas_id: params[:id], fk_status_monitoria_id: 3)
+    turma = quantidade.to_i
+    puts turma
+    return true if @monitoria.length <= turma || @monitoria.length == 0
+  end
 end
