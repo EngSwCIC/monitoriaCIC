8e45120aff77a173d3f1aae15d982d1d22b08a45
JonFrancis <71151934+JonFrancis@users.noreply.github.com>
2021-05-19 21:04:10 -0300

    Refatoração e Documentação dos métodos

--- app/controllers/monitorias_remunerada_controller.rb
+++ app/controllers/monitorias_remunerada_controller.rb
@@ -1,39 +1,44 @@
 class MonitoriasRemuneradaController < ApplicationController
 	before_action :find_monitor, only: [:show]
 	before_action :find_monitoria, only: [:destroy, :edit, :update]
-	before_action :logged_in
 
 	def show
 		@alunos = User.find_each
 	end
 
-	def edit	
-	end
+	##
+	# Método para dar update nas vagas de monitoria, ao atualizar a situação
+	# da monitoria, atualiza a quantidade de bolsas de cada turma.
+	# Por fim redireciona a página para a aba de monitoria remunerada.
 
 	def update
-    @turmas = Turma.all
-    situacao = 0
-    
-    if @monitoria.fk_status_monitoria_id == 1 || @monitoria.fk_status_monitoria_id == 3
-      situacao = 1
-    elsif @monitoria.fk_status_monitoria_id == 2
-      situacao = 2
-    end
+		@turmas = Turma.all
+		status = @monitoria.fk_status_monitoria_id
+		turma_id = @monitoria.fk_turmas_id
 
-    @turmas.each do |turma|
-      if turma.id == @monitoria.fk_turmas_id
-        if situacao == 1
-          turma.update!(qnt_bolsas: turma.qnt_bolsas + 1)
-        elsif situacao == 2
-          turma.update!(qnt_bolsas: turma.qnt_bolsas - 1)
-        end
-      end
-    end
+		case status
+		when 1 || 2 
+			situacao = 1
+		else
+			situacao = 2
+		end
+
+		@turmas.each do |turma|
+			if turma.id == turma_id
+				bolsas = turma.qnt_bolsas
+				case situacao
+				when 1
+					turma.update!(qnt_bolsas: bolsas + 1)
+				else
+					if(bolsas > 0)
+						turma.update!(qnt_bolsas: bolsas - 1)
+					end
+				end
+			end
+		end
 
 		if @monitoria.update monitoria_params
 			redirect_to dashboard_monitoria_remunerada_path, notice: "Situaçao atualizada!"
-		else
-			render 'edit'
 		end
 	end
 
@@ -42,17 +47,11 @@ class MonitoriasRemuneradaController < ApplicationController
 		params.require(:monitoria).permit(:remuneracao, :fk_matricula, :fk_cod_disciplina, :fk_turmas_id, :descricao_status, :prioridade, :fk_status_monitoria_id)
 	end
 
-	def find_monitor
+		def find_monitor
 		@monitoria = Monitoria.where(fk_turmas_id: params[:id])
 	end
-
+	
 	def find_monitoria
 		@monitoria = Monitoria.find(params[:id])
-	end
-
-	def logged_in
-		if !logged_in?
-			redirect_to new_session_path, notice: "Você precisa estar logado para acessar a pagina Monitoria"
-		end
 	end
 end
